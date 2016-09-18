b1dd0aed8eb82b7505c71bf1d837c753feb7b23e
Bill Hefty <billhefty@gmail.com>
2016-09-18 09:53:21 -0500

    added routes

--- app/index.js
+++ app/index.js
@@ -1,12 +1,5 @@
-let React = require('react')
-let ReactDOM = require('react-dom')
+const React = require('react')
+const ReactDOM = require('react-dom')
+const routes = require('../config/routes');
 
-const HelloWorld = React.createClass({
-  render () {
-    return (
-        <div><h1>Hello World!</h1></div>
-    )
-  }
-})
-
-ReactDOM.render(<HelloWorld />, document.getElementById('app'))
+ReactDOM.render(routes, document.getElementById('app'))
---
+++ components/Main.js
@@ -0,0 +1,11 @@
+const React = require('react')
+
+const Main = React.createClass({
+  render () {
+    return (
+        <div><h1>Hello World!</h1></div>
+    )
+  }
+})
+
+module.exports = Main
